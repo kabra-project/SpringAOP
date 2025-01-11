9bbdc304f5c335d5ce890f95feed57a7dd6907e4
kabra-project <kabra@gmx.net>
2025-01-11 17:11:46 +0100

    Step 7

--- src/main/java/com/in28minutes/learn_spring_aop/aopexample/aspects/LoggingAspect.java
+++ src/main/java/com/in28minutes/learn_spring_aop/aopexample/aspects/LoggingAspect.java
@@ -12,21 +12,21 @@ public class LoggingAspect {
 
     private Logger logger = LoggerFactory.getLogger(getClass());
 
-    @Before("execution (* com.in28minutes.learn_spring_aop.aopexample.*.*.*(..))")
+    @Before("execution (* com.in28minutes.learn_spring_aop.aopexample.business.*.*(..))")
     public void logMethodCallBevorExecution(JoinPoint joinPoint){
 
         logger.info("Before Aspect - {} is called with the following Arguments: {}", joinPoint, joinPoint.getArgs());
 
     }
 
-    @After("execution (* com.in28minutes.learn_spring_aop.aopexample.*.*.*(..))")
+    @After("execution (* com.in28minutes.learn_spring_aop.aopexample.business.*.*(..))")
     public void logMethodCallAfterExecution(JoinPoint joinPoint){
 
         logger.info("After Aspect - {} has been executed", joinPoint);
 
     }
 
-    @AfterThrowing(pointcut = "execution (* com.in28minutes.learn_spring_aop.aopexample.*.*.*(..))",
+    @AfterThrowing(pointcut = "execution (* com.in28minutes.learn_spring_aop.aopexample.business.*.*(..))",
                     throwing = "exception")
     public void logMethodCallAfterThrowing(JoinPoint joinPoint, Exception exception){
 
@@ -34,10 +34,10 @@ public class LoggingAspect {
 
     }
 
-    @AfterReturning(pointcut =  "execution (* com.in28minutes.learn_spring_aop.aopexample.*.*.*(..))", returning = "result value")
-    public void logMethodCallAfterReturning(JoinPoint joinPoint, Object resultValue){
+    @AfterReturning(pointcut =  "execution (* com.in28minutes.learn_spring_aop.aopexample.business.*.*(..))", returning = "result")
+    public void logMethodCallAfterReturning(JoinPoint joinPoint, int result){
 
-        logger.info("After Successful Excecution - {} has returned", joinPoint, resultValue);
+        logger.info("After Successful Excecution - {} has returned {}", joinPoint, result);
 
     }
 }
